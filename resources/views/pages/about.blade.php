42e14c7ff21d4d8ebd8bc6e749840345c9ce1295
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-09-05 11:01:02 +0100

    [feature] laravelblog

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>THis is about page </h1>
+
+@endsection
---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>THis is services page </h1>
+
+@endsection
