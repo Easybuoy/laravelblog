e89f12f2a0d34e53f2dcebff302e74ededc69b49
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-09-06 05:09:39 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-$url = parse_url(getenv('DATABASE_URL'));
+$url = parse_url(getenv('DATABASE_URL')); var_dump($url);
 $host = $url['host']??null;
 $username = $url['user']??null;
 $password = $url['pass']??null;
