4861c23fe7af0758071ef6b185dc9d3e162cd6ee
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-09-06 04:25:52 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Contracts\Routing\UrlGenerator;
-use Symfony\Component\Routing\Generator\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
